3589da601b645cd0740137dd0a712e1ce9772b3c
Solotole <solomontoleak47@gmail.com>
2024-10-24 18:21:31 +0100

    how to update document in mongo

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -23,36 +23,44 @@ class DBStorage:
     def all(self, cls=None):
         """ querrying through a database """
         new_dict = {}
-        for clss in classes:
-            if cls is None or cls is classes[clss] or cls is clss:
-                collection_name = cls.__name__.lower()
-                objs = db[collection_name].find()
-                for obj in objs:
-                    key = f"{cls.__name__}.{obj['id']}"
-                    # key = obj.__class__.__name__ + '.' + obj.id
-                    new_dict[key] = cls(**obj)
+        if cls:
+            collection_name = cls.__name__.lower()
+            objs = db[collection_name].find()
+            for obj in objs:
+                key = f"{cls.__name__}.{obj['id']}"
+                # key = obj.__class__.__name__ + '.' + obj.id
+                new_dict[key] = cls(**obj)
         return new_dict
 
     def new(self, obj):
         """ add the object to the current database session """
         new_document = db[obj.__class__.__name__.lower()].insert_one(obj.to_dict())
 
-    # def save(self):
-    # """ commit all changes of the current database session """
-    # db[str(self.__class__.__name__)].insert_one(self.to_dict())
-
-    def delete(self, obj=None):
+    def delete(self, cls, obj_id):
         """ delete from the current database session obj """
-        if obj:
-            db[obj.__class__.__name__.lower()].delete_one({"id": obj.id})
+        if cls and obj_id:
+            db[classes[cls].__name__.lower()].delete_one({"id": obj_id})
 
     def get(self, cls, id):
         """  method to retrieve one object """
         if not cls or not id:
             return None
         all_dicts = {}
+        if isinstance(cls, str):
+            cls = classes[cls]
         collection_name = cls.__name__.lower()
         obj_dict = db[collection_name].find_one({"id": id})
         if obj_dict:
             return cls(**obj_dict)
         return None
+
+    def update(self, cls, doc_id, data):
+        """ method to carry out update of data/document in collection"""
+        if not cls or not doc_id or not data:
+            return none
+        if not self.get(cls, doc_id):
+            return None
+        if isinstance(cls, str):
+            cls = classes[cls]
+        collection_name = cls.__name__.lower()
+        db[collection_name].update_one({"id": doc_id}, {"$set": data})
