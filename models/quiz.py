3d00050cc262922a5d4bd2f9ae4cd8ae5bc343e8
Solotole <solomontoleak47@gmail.com>
2024-11-02 07:31:56 +0000

    quiz model

--- models/quiz.py
+++ models/quiz.py
@@ -3,6 +3,9 @@ from models.base_model import BaseModel, db
 
 class Quiz(BaseModel):
         """ Represents a Quiz object in the system """
-        def __init__(self, *args, **kwargs):
+        def __init__(self,title, description, *args, **kwargs):
             """ Initializes a quiz """
             super().__init__(*args, **kwargs)
+            self.title = title
+            self.description = description
+            self.questions = []
