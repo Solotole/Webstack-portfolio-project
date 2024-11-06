52a7fc61aa11a3edd7ab69a9393aa911f95550d2
Solotole <solomontoleak47@gmail.com>
2024-11-06 08:26:55 +0000

    quiz validation

--- web_dynamic/quiz.py
+++ web_dynamic/quiz.py
@@ -6,13 +6,33 @@ from flask import Blueprint
 from models import storage
 from models.answer import Answer
 from models.question import Question
+from models.result import Result
+
 
 tasks_bp = Blueprint('task', __name__)
+
+
+def non_attempted():
+    """ retrieving unattempted quizzes"""
+    attempted_quizzes = []
+    results = storage.all(Result)
+    for value in results.values():
+        if value.user_id == session.get('id'):
+            attempted_quizzes.append(value.id)
+    return attempted_quizzes
+
 
 @tasks_bp.route('/show_quizzes')
 def show_quizzes():
     """retrieving all quizzes"""
     if not session.get('loggedin', None):
         return redirect('/login')
-    quizzes = storage.all(Quiz)
+    attempted = non_attempted()
+    quizz = storage.all(Quiz)
+    quizzes = {}
+    for key, value in quizzes.items():
+        if value.id in attempted:
+            continue
+        else:
+            quizzes[key] = value
     return render_template('quiz.html', quizzes=quizzes)
